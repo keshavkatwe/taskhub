8fc72a5049b32192d9844f904721a1bd18fb0252
Keshav Katwe <keshav.katwe@gmail.com>
2024-09-25 18:53:17 +0400

    fixed font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,25 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    fontSize: {
+      display1:"48px",
+      display2:"40px",
+      display3:"32px",
+      display4:"26px",
+      display5:"22px",
+      display6:"18px",
+      display7:"16px",
+      h1:"48px",
+      h2:"32px",
+      h3:"28px",
+      h4:"24px",
+      h5:"20px",
+      h6:"18px",
+      "body-large":"16px",
+      "body-medium":"14px",
+      "body-small":"12px",
+      "body-x-small":"10px"
+    }
   },
   plugins: [],
 };
