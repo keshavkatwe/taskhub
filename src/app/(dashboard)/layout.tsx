f99a352df7386a600f97a4a9c4d164acb1e7c1de
Keshav Katwe <keshav.katwe@gmail.com>
2024-09-26 21:11:41 +0400

    Added layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,23 @@
 import { PropsWithChildren } from "react";
+import Image from "next/image";
+import Logo from "@/assets/logo.svg";
 
 const DashboardLayout = ({ children }: PropsWithChildren) => {
-  return <div>{children}</div>;
+  return (
+    <div className={"flex h-screen"}>
+      <div className={"w-72"}>
+        <Image src={Logo} alt={""} />
+      </div>
+      <div className={"flex-grow p-2"}>
+        <div
+          className={
+            "rounded-br-16 bg-background-white h-full border border-gray-50"
+          }
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default DashboardLayout;
