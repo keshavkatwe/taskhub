95e63d57ead6f472db7fe5f964a007c495315359
Keshav Katwe <keshav.katwe@gmail.com>
2024-09-26 22:13:02 +0400

    Added icons

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,4 +1,10 @@
+import { CaretUpDown } from "@phosphor-icons/react/dist/ssr";
+
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <h1>
+      Dashboard <CaretUpDown className="w-5 h-5 text-gray-500" weight="bold" />
+    </h1>
+  );
 };
 export default Dashboard;
