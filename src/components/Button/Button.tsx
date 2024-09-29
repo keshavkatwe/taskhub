b8f9ccf7ce99b208cde92b876ed3ce8f5434884b
Keshav Katwe <keshav.katwe@gmail.com>
2024-09-29 08:51:10 +0400

    Added loading

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,24 @@
 import { ButtonHTMLAttributes, PropsWithChildren } from "react";
+import { CircleNotch } from "@phosphor-icons/react/dist/ssr";
 
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isLoading?: boolean;
+}
 const Button = ({
   children,
+  isLoading,
+  disabled,
   ...otherProps
-}: PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) => {
+}: PropsWithChildren<IButtonProps>) => {
   return (
     <button
       {...otherProps}
       className={
-        "bg-gradient-to-r from-blue-500 to-blue-600 h-12 rounded-br-8 text-text-white text-body-large font-semibold drop-shadow-primary-button px-4"
+        "bg-gradient-to-r from-blue-500 to-blue-600 h-12 rounded-br-8 text-text-white text-body-large font-semibold drop-shadow-primary-button px-4 flex items-center gap-1 disabled:opacity-50"
       }
+      disabled={disabled || isLoading}
     >
+      {isLoading && <CircleNotch size={18} className={"animate-spin"} />}
       {children}
     </button>
   );
